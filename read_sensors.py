a91622aa0320c1d80eaad252f820b319495988e1
klarrieu <kennylarrieu@gmail.com>
2023-07-15 13:25:44 -0700

    added sensor logging, bugfix for weather_update

--- read_sensors.py
+++ read_sensors.py
@@ -16,9 +16,13 @@ import time
 from numpy import mean
 import numpy as np
 from read_config import read_config
+import logging
 
+
+logging.basicConfig(filename='sensors.log', encoding='utf-8', level=logging.DEBUG)
 
 installed_devices, device_configs = read_config()
+getters = {device: globals()[device_configs[device]["getter"]] for device in installed_devices}
 
 # prepare database for I/O
 setup_db()
@@ -30,21 +34,25 @@ readings = []
 t1 = time.time()
 
 while True:
-  # get sensor readings
-  row = []
-  for device in installed_devices:
-    device_reading = locals()[device_configs[device]["getter"]]()
-    row.extend(device_reading)
-  readings.append(row)
-  # get current time
-  t2 = time.time()
-  # if we've been reading values for 60 seconds
-  if t2 - t1 >= 60:
-    # reset clock
-    t1 = time.time()
-    # get mean values from last 60 seconds
-    readings = tuple(np.array(readings).mean(axis=0))
-    # add mean values to database
-    add_entry(readings)
-    # reset array
-    readings = []
+  try:
+    # get sensor readings
+    row = []
+    for device in installed_devices:
+      device_reading = getters[device]()
+      row.extend(device_reading)
+    readings.append(row)
+    # get current time
+    t2 = time.time()
+    # if we've been reading values for 60 seconds
+    if t2 - t1 >= 60:
+      # reset clock
+      t1 = time.time()
+      # get mean values from last 60 seconds
+      readings = tuple(np.array(readings).mean(axis=0))
+      # add mean values to database
+      add_entry(readings)
+      # reset array
+      readings = []
+  except Exception as e:
+    # log error and try again
+    logging.error(e)
